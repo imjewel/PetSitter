c308e3798255242f84e63f215f556c930a7e9ed4
GREEN <GREEN@G1101-06>
2023-04-06 17:45:06 +0900

    yurim commit test auth 수정 필요

--- PetSitter/src/main/java/com/cos/petsitter/controller/BoardController.java
+++ PetSitter/src/main/java/com/cos/petsitter/controller/BoardController.java
@@ -49,7 +49,7 @@ public class BoardController {
 	}
 	
 	//USER권한이 필요
-	@GetMapping({"/auth/board/saveForm"}) 
+	@GetMapping({"/board/saveForm"}) 
 	public String saveForm() { 
 		return "board/saveForm";
 	}
